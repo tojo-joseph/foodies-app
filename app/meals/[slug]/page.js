dfe3fd0d592af538e11de1dcb0bd4951e9854ab8
tojo-joseph <tojo.joseph18@gmail.com>
2024-08-05 11:59:36 +0530

    new error, dynamic and not found pages added

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,12 +1,40 @@
 import Link from "next/link";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealPost({ params }) {
+  const meal = getMeal(params.slug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  if (!meal) {
+    notFound();
+  }
+
   return (
-    <main>
-      <p>Meal</p>
-      <p>
-        <Link href="/meals">Go to Meals Page</Link>
-      </p>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.name}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
